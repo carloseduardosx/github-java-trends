66cd5a6c330d8e82266f3b32e236dbbaa63b0791
Carlos Eduardo <carloseduardosx2015@gmail.com>
2016-12-08 09:56:02 -0300

    Buffer deletion emission
    Bufferring data deletion emissions to avoid multiple items emission at
    flapMap operator

--- app/src/main/java/com/carloseduardo/github/base/BaseActivity.java
+++ app/src/main/java/com/carloseduardo/github/base/BaseActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.carloseduardo.github.application.GitHubTrendApplication;
-import com.carloseduardo.github.injector.component.ApplicationComponent;
-
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
